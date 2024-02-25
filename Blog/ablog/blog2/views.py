634e29634e5e7db1c99ff566626e587ff7c9e5bf
jaydenwitucki <jaydenwitucki@gmail.com>
2024-02-25 00:54:35 -0500

    added changes

--- Blog/ablog/blog2/views.py
+++ Blog/ablog/blog2/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
 
+from .forms import PostForm, EditForm
 from .models import Post
 
 # Create your views here.
@@ -17,5 +18,13 @@ class ArticleDetailView(DetailView):
     
 class AddPostView(CreateView):
     model = Post
+    form_class = PostForm
     template_name = 'add_post.html'
-    fields = '__all__'
+    #fields = '__all__'
+    
+class UpdatePostView(UpdateView):
+    model = Post
+    form_class = EditForm
+    template_name = 'update_post.html'
+    #fields = ['title', 'title_tag', 'body']
+    
