cc5c1093f3ad13f2c68ad8379c21a54ba5a44b13
jaydenwitucki <jaydenwitucki@gmail.com>
2024-08-13 11:28:52 -0400

    first commit- added top bar

---
+++ NBA_react-app/nba-react-app/src/components/App.js
@@ -0,0 +1,14 @@
+//import logo from './logo.svg';
+import TopNavBar from './TopNavBar';
+
+function App() {
+  return (
+    <div className="App">
+      <TopNavBar />
+
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ NBA_react-app/nba-react-app/src/components/TopNavBar.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+//import logo from "../assets/images/nba-logoman-word-white.svg";
+
+class TopNavBar extends Component {
+    render() {
+        return (
+            <header className="App-header">
+                <img src="nba-6.svg" className="App-logo" alt="logo" />
+            </header>
+        );
+    }
+}
+
+export default TopNavBar;
